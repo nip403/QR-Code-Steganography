3417b5b2abf28f2546fa8521330ee149f60204ee
nip403 <41174986+nip403@users.noreply.github.com>
2019-11-10 15:06:21 +0000

    Major improvements to encoding & decoded processes

--- engine.py
+++ engine.py
@@ -4,14 +4,9 @@ import numpy as np
 import os.path
 import sys
 
-# encodes by least significant bit (x % 2 == 1, white; x % 2 == 0, black)
-# currently only supports rgb images
-
-#####NOTE: scale produced qr codes up to 500 px minimum
 ######NOTE::::: fix compatibility for jpg imgs
 ###NOTE: add compatibility for other image formats
 ###NOTE: add extra option to rename file + in steg.py
-
 
 class Encoder:
     default = "default.png"
@@ -86,29 +81,29 @@ class Encoder:
 
     def _encode_pix(self, val, pix):
         if val: # data = 1
-            if not (pix[0] % 2): # check if even
-                pix[0] += 1
+            if not (pix % 2): # check if even
+                pix += 1
         else: # data = 0
-            if pix[0] % 2: # check if odd
-                pix[0] -= 1
+            if pix % 2: # check if odd
+                pix -= 1
 
-        return tuple(pix)
+        return pix
 
     def _encode_dim(self, qr, img_data):
         # top row encodes size of qr code
         # only 1 val needed as qr codes are always square
         top = self._convert_bin(qr.shape[0])
-        top = [0]*(img_data.shape[1] - len(top)) + top
+        top = np.array([0]*(img_data.shape[1] - len(top)) + top)
 
         # convert first line into binary which encodes qrcode length
         actual_top = np.reshape(np.array(self.img.crop((0, self._length_data_line, self.img.size[0], self._length_data_line+1))), (img_data.shape[1], 3))
 
         # encode binary into red channel
-        new_top = tuple(self._encode_pix(top[i], actual_top[i]) for i in range(img_data.shape[1]))
+        new_top = np.concatenate((np.reshape(np.vectorize(self._encode_pix)(top, actual_top[:, 0]), (len(top), 1)), actual_top[:, 1:]), axis=1)
 
         # create new image, overwrite top line with formatted line
         img_top = Image.new(self.img.mode, (self.img.size[0], 1))
-        img_top.putdata(new_top)
+        img_top.putdata([tuple(i) for i in new_top])
         self.img.paste(img_top, (0, 0))
 
     def encode(self, data, dest_dir=".", show_input=False, show_qr=False, show_result=False):
@@ -124,13 +119,8 @@ class Encoder:
 
         # encodes qr code, topleft=(0,0)
         qr_img = np.array(self.img.crop((0, 0, *qr.shape)))
-        for y in range(qr_img.shape[0]):
-            for x in range(qr_img.shape[1]):
-                qr_img[y, x] = self._encode_pix(qr[y, x], qr_img[y, x])
-        #qr_img = np.apply_along_axis(self._encode_dim, 2, np.reshape(qr, (list(qr.shape) + [1]), qr_img))
-        #qr_img = np.apply_over_axes(self._encode_dim, np.reshape(qr, (list(qr.shape) + [1]), qr_img), [1, 3])
+        qr_cover = Image.fromarray(np.concatenate((np.reshape(np.vectorize(self._encode_pix)(qr, qr_img[:, :, 0]), (*qr.shape, 1)), qr_img[:, :, 1:]), axis=2))
 
-        qr_cover = Image.fromarray(qr_img[1:])
         self.img.paste(qr_cover, (0, 1))
         self.img.save(f"{dest_dir}\{self._name}-hidden{self._ext}")
 
@@ -139,11 +129,10 @@ class Encoder:
 
         return self.img, f"{dest_dir}\{self._name}-hidden{self._ext}"
 
-##### NOTE: np.array % 2 returns everything i needdddddd, replace existing method soon
-
 class Decoder:
     default = "default-hidden.png"
     default_dir = "."
+    _min_out_size = 500
 
     def __init__(self, img=None):
         self.img = img if (img is not None and not os.path.isfile(img)) else self.default
@@ -176,48 +165,29 @@ class Decoder:
             self.img = Image.open(self.img)
         except:
             raise FileNotFoundError(f"'{self._name+self._ext}' was not found in {sys.path}.\nPlease check if the file exists.")
- 
-    def _decode_pix_to_bin(self, pix):
-        return pix[0] % 2
-
-    def _decode_pix(self, pix):
-        return (pix[0] % 2) * 255
-
-    def _convert_dec(self, arr):
-        b = int("".join(map(str, arr)))
-        out = 0
-        power = 0
-
-        while b:
-            rem = b % 10
-            out += 2**power * rem
-            b //= 10
-            power += 1
-
-        return out
 
     def decode(self, dest_dir=".", show=False):
         if not os.path.isdir(dest_dir):
             dest_dir = self.default_dir
 
         # scrapes top line of pixels form input, converts to binary from least significant bit to find dimensions of qr code
-        qr_dim = self._convert_dec(list(map(self._decode_pix_to_bin, np.reshape(np.array(self.img.crop((0, 0, self.img.size[0], 1))), (self.img.size[0], 3)))))
-        ###NOTE: delete above function when below is fixed
-        ###NOTE: add check to see is qrdim > img.size to avoid dos attack error
+        qr_dim = int("".join((np.array(self.img.crop((0, 0, self.img.size[0], 1)))[:, :, 0][0] % 2).astype(str)), 2)
 
-        print(qr_dim)
+        # min size of qr = 500 (defined as base attr)
+        # if qr_dim > 500, scale_factor will always be 1
+        scale_factor = int(np.ceil(self._min_out_size/qr_dim))
 
         # get qr code, and parse for lsb
         try:
-            qr_raw = np.insert(np.array(self.img.crop((0, 0, qr_dim, qr_dim)))[1:], 0, np.ones((1, qr_dim, 3)), 0)
+            qr = Image.fromarray((np.insert(np.array(self.img.crop((0, 0, qr_dim, qr_dim)))[1:], 0, np.ones((1, qr_dim, 3)), 0)[:, :, 0] % 2 * 255).astype("uint8"), mode="L")
         except:
             raise ValueError("Unformatted file, retry with encoded file.")
 
-        qr_img = np.apply_along_axis(self._decode_pix, 2, qr_raw)
-        #NOTE:::::NOTE!!!! - image not saving properly
+        # NOTE: update to use python 3.8 walrus operator
+        # resize to min size (as defined in base attrs)
+        if qr.size[0] < 500:
+            qr = qr.resize(list(map(lambda i: int(np.ceil(self._min_out_size/i)) * i, qr.size)))
 
-        #NOTE: fix this for 1 bit numbers (mode="1")
-        qr = Image.fromarray(qr_img.astype("uint8"), mode="L")
         qr.save(f"{dest_dir}\{self._name}-qr{self._ext}")
 
         if show:
